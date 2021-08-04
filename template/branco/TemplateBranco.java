6e5b4dd08f27f751075b8f1951056d1deda7e709
LUCAS GOES DE SOUZA <lucas.souza13@alunos.sc.senac.br>
2021-08-03 21:58:42 -0300

    Versão 2.0

--- template/branco/TemplateBranco.java
+++ template/branco/TemplateBranco.java
@@ -11,29 +11,29 @@ public class TemplateBranco {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String str1 = " Olá Mundo ";
+        String str1 = " Olá Mundo \n";
 int[] ListNum = {1, 2, 3, 4};
 int resp = 5;
         System.out.print(str1);
         if(ListNum[0] + ListNum[1] == resp){
             System.out.printf(" %d + ", ListNum[0]);
             System.out.printf(" %d = ", ListNum[1]);
-            System.out.printf(" %d ",  resp);
+            System.out.printf(" %d \n",  resp);
         } 
         if(ListNum[0] + ListNum[2] == resp){
             System.out.printf(" %d + ", ListNum[0]);
             System.out.printf(" %d = ", ListNum[2]);
-            System.out.printf(" %d ",  resp);
+            System.out.printf(" %d \n",  resp);
         }
         if(ListNum[0] + ListNum[3] == resp){
             System.out.printf(" %d +", ListNum[0]);
             System.out.printf(" %d = ", ListNum[3]);
-            System.out.printf(" %d ", resp);
+            System.out.printf(" %d \n", resp);
         }
         if(ListNum[1] + ListNum[2] == resp){
-            System.out.printf(" %d + ", ListNum[1]);
+            System.out.printf(" %d +", ListNum[1]);
             System.out.printf(" %d = ", ListNum[2]);
-            System.out.printf(" %d ",  resp);
+            System.out.printf(" %d \n",  resp);
         } 
      else {
              System.out.print("Resposta errada");
